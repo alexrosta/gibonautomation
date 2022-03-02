272f39925a67d7ccefe701b7929413b23930aa4c
alexrosta <99803826+alexrosta@users.noreply.github.com>
2022-03-02 16:45:06 +0100

    Delete newcomputer.ps1

--- newcomputer.ps1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem -Path D:\DESK -Recurse | Move-Item -Destination C:\Users\%USERPROFILE%\Desktop
-Get-ChildItem -Path D:\DOC -Recurse | Move-Item -Destination C:\Users\%USERPROFILE%\Documents
-Get-ChildItem -Path D:\PIC -Recurse | Move-Item -Destination C:\Users\%USERPROFILE%\Pictures
-Get-ChildItem -Path D:\Outlook -Recurse | Move-Item -Destination C:\Users\%USERPROFILE%\AppData\Local\Microsoft\Outlook
